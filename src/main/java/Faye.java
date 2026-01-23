962f05f50f3278a77d5495fa10eade59c8ea326f
feiyiwang <e1355846@u.nus.edu>
2026-01-23 08:58:42 +0800

    Echo in African American Style

--- src/main/java/Faye.java
+++ src/main/java/Faye.java
@@ -1,10 +1,30 @@
+import  java.util.Scanner;
+
 public class Faye {
     public static void main(String[] args) {
         System.out.println("____________________________________________");
-        System.out.println(" Hello! I'm Faye");
-        System.out.println(" What can I do for you?");
+        System.out.println(" Yo Wassup my G! Yo friend Faye right here");
+        System.out.println(" What can I do for you my dawg?");
         System.out.println("____________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________");
+        System.out.println(" Nah. I can do no nothing other than repeating inputs. SIKE!");
+        System.out.println(" Now enter yo mf inputs: ");
+
+
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            String input = scanner.nextLine();
+
+            if(input.equals("bye")) {
+                System.out.println("____________________________________________");
+                System.out.println(" Im outta here. Peace!");
+                System.out.println("____________________________________________");
+                break;
+            }
+
+            System.out.println(input);
+            System.out.println("____________________________________________");
+        }
+
+
     }
 }
