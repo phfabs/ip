396bb4acfb0850dc09a116168d6ce3497c0c60dd
feiyiwang <e1355846@u.nus.edu>
2026-01-28 17:30:13 +0800

    Add List feature

--- src/main/java/Faye.java
+++ src/main/java/Faye.java
@@ -1,4 +1,5 @@
 import  java.util.Scanner;
+import java.util.ArrayList;
 
 public class Faye {
     public static void main(String[] args) {
@@ -6,11 +7,15 @@ public class Faye {
         System.out.println(" Yo Wassup my G! Yo friend Faye right here");
         System.out.println(" What can I do for you my dawg?");
         System.out.println("____________________________________________");
-        System.out.println(" Nah. I can do no nothing other than repeating inputs. SIKE!");
-        System.out.println(" Now enter yo mf inputs: ");
+        System.out.println(" As a skilled person, i can decument yo inputs");
+        System.out.println(" Now enter yo mf inputs if u wna test me out: ");
 
 
         Scanner scanner = new Scanner(System.in);
+
+        //Stores user inputs
+        ArrayList<String> inputList = new ArrayList<>();
+
         while(true){
             String input = scanner.nextLine();
 
@@ -19,10 +24,23 @@ public class Faye {
                 System.out.println(" Im outta here. Peace!");
                 System.out.println("____________________________________________");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("____________________________________________");
+
+                for (int i = 0; i < inputList.size(); i++){
+                    System.out.println((1 + i) + "." + inputList.get(i));
+                }
+
+                System.out.println("____________________________________________");
+            } else {
+                inputList.add(input);
+                System.out.println("____________________________________________");
+                System.out.println(" added: " + input);
+                System.out.println("____________________________________________");
+
             }
 
-            System.out.println(input);
-            System.out.println("____________________________________________");
+           
         }
 
 
